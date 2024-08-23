480f5448ee114471540a1003421cb12db23f069e
DevMoodie <Mahmoudyf96@gmail.com>
2024-08-23 12:21:36 -0400

    Initialize Habit Tracker project with SwiftUI

---
+++ Habit Tracker/Views/HabitTrackerView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitTrackerView.swift
+//  Habit Tracker
+//
+//  Created by Moody on 2024-08-23.
+//
+
+import SwiftUI
+
+struct HabitTrackerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitTrackerView()
+}
